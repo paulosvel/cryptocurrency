954cb12f9801756eddda1b06bbaf923d24c77a41
paulosvel <paul2001vel@gmail.com>
2023-07-17 14:33:16 +0300

    added stylings

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import './Styling.css'
 function CoinItem(props) {
   return (
-    <Box>
+    <Box className="lines" sx={{display:"flex", justifyContent:"space-between", alignItems:"center", margin:"2rem 1rem",padding:".7rem 1rem", backgroundColor:"#26272b", boxShadow:"0px 0px 12px #18191b", borderRadius:"8px"}}>
       <p>{props.coins.market_cap_rank}</p>
-      <Box>
-        <img src={props.coins.image} />
-        <p>{props.coins.symbol}</p>
+      <Box  sx={{display:"flex", alignItems:"center",}} >
+        <img style={{height:"40px", marginRight:"8px", width:"auto"}} src={props.coins.image} />
+        <p >{props.coins.symbol}</p>
       </Box>
-      <p>{props.coins.current_price}</p>
-      <p>{props.coins.price_change_percentage_24h}</p>
-      <p>{props.coins.total_volume}</p>
-      <p>{props.coins.market_cap}</p>
+      <p>{props.coins.current_price}€</p>
+      <p>{props.coins.price_change_percentage_24h}%</p>
+      <p>{props.coins.total_volume}€</p>
+      <p>{props.coins.market_cap}€</p>
     </Box>
   )
 }
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -5,11 +5,11 @@ const Coins = (props) => {
   const coins = props.coins;
 
   return (
-    <Box>
+    <Box sx={{maxWidth:"1000px", margin:"auto"}}>
       <Box>
-        <Box>
-          <p>#</p>
-          <p>Coin</p>
+        <Box sx={{display:"flex", justifyContent:"space-between", alignItems:"center", margin:"2rem 1rem", padding:".7rem 1rem", backgroundColor:"#26272b", boxShadow:"0px 0px 12px #18191b", borderRadius:"8px"}}>
+          <p >#</p>
+          <p style={{marginLeft:"4px"}}>Coin</p>
           <p>Price</p>
           <p>24h</p>
           <p>Volume</p>
