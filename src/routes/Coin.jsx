0ef7517b8f1d4a91114a800a523f0ea8f66dcdbe
paulosvel <paul2001vel@gmail.com>
2023-07-18 11:08:19 +0300

    added coin paths with info inside each one

--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material'
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import {useParams} from 'react-router-dom';
+import DOMPurify from 'dompurify';
 
 function Coin() {
   const [coin , setCoin] = useState ({});
@@ -34,8 +35,7 @@ function Coin() {
       <p>{coin.symbol}</p>        
       </Box>
       <Box>
-        {coin.market_data ? <h1>{coin.market_data.current_price}</h1> : null}
-      <h1>{coin.market_data.current_price}</h1>
+      {coin.market_data?.current_price ? <h1>${coin.market_data.current_price.eur.toLocaleString()}</h1> : null}
       </Box>
     </Box>
   </Box>
@@ -54,12 +54,12 @@ function Coin() {
       </thead>
       <tbody>
         <tr>
-          <td>{coin.market_data.price_change_percentage_1h_in_currency.eur}</td>
-          <td>{coin.market_data.price_change_percentage_24h_in_currency.eur}</td>
-          <td>{coin.market_data.price_change_percentage_7d_in_currency.eur}</td>
-          <td>{coin.market_data.price_change_percentage_14d_in_currency.eur}</td>
-          <td>{coin.market_data.price_change_percentage_30d_in_currency.eur}</td>
-          <td>{coin.market_data.price_change_percentage_1y_in_currency.eur}</td>
+        <td>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.eur.toFixed(1)}%</p> : null}</td>
+        <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_24h_in_currency.eur.toFixed(1)}%</p> : null}</td>
+        <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_7d_in_currency.eur.toFixed(1)}%</p> : null}</td>
+        <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_14d_in_currency.eur.toFixed(1)}%</p> : null}</td>
+        <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_30d_in_currency.eur.toFixed(1)}%</p> : null}</td>
+        <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_1y_in_currency.eur.toFixed(1)}%</p> : null}</td>
         </tr>
       </tbody>
     </table>
@@ -69,21 +69,23 @@ function Coin() {
     <Box>
       <Box>
 <h4>24 Hour Low</h4>
-<p>{coin.market_date.low_24.eur}</p>
+{coin.market_data?.low_24h ? <p>${coin.market_data.low_24h.eur.toLocaleString()}</p> : null}
 </Box>
 <Box>
 <h4>24 Hour High</h4>
-<p>{coin.market_date.high_24.eur}</p>
+{coin.market_data?.high_24h ? <p>{coin.market_data.high_24h.eur.toLocaleString()}</p> : null}
 </Box>
     </Box>
     <Box>
     <Box>
 <h4>Market Cap</h4>
-<p>{coin.market_date.market_cap.eur}</p>
+
+{coin.market_data?.market_cap ? <p>${coin.market_data.market_cap.eur.toLocaleString()}</p> : null}
+
 </Box>
 <Box>
 <h4>Circulating Supply</h4>
-<p>{coin.market_date.circulating_supply}</p>
+{coin.market_data ? <p>{coin.market_data.circulating_supply}</p> : null}
 </Box>
     </Box>
 
@@ -93,7 +95,11 @@ function Coin() {
   <Box>
     <Box>
       <h3>About</h3>
-      <p>{coin.description.en}</p>
+      <p dangerouslySetInnerHTML={{
+        __html: DOMPurify.sanitize(coin.description ? coin.description.en : ''),
+       }}>
+
+    </p>
     </Box>
   </Box>
 </Box>
@@ -101,4 +107,4 @@ function Coin() {
   )
 }
 
-export default Coin
+export default Coin;
